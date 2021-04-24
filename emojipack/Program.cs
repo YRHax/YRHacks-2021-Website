ea7d45f1be3d29790f68f34bf6ab5d8835929c81
encodeous <encodeous7@gmail.com>
2021-04-23 20:31:07 -0400

    Pogress made!

---
+++ emojipack/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace emojipack
+{
+    public class Program
+    {
+        public static string ApiUrl = "https://api.emojipack.cf";
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices(x =>
+            {
+                x.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+            });
+            builder.Services.AddBlazoredLocalStorage();
+            await builder.Build().RunAsync();
+        }
+    }
+}
